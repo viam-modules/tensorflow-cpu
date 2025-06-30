59070f359ac5d6abbfa76c2940fa9264d53caab1
Khari Jarrett <khari@viam.com>
2025-06-30 14:34:32 -0400

    really fix warning

--- src/tensorflow_module.py
+++ src/tensorflow_module.py
@@ -47,7 +47,7 @@ class TensorflowModule(MLModel, Reconfigurable):
         # If it's a Keras model file, okay. Otherwise, it must be a SavedModel directory
         _, ext = os.path.splitext(model_path)
         if ext.lower() == ".keras":
-            LOGGER.warn("Detected Keras model file at ", model_path, ". Please note that Keras support is limited.")
+            LOGGER.info("Detected Keras model file at " + model_path + ". Please note Keras support is limited.")
             return []
         
         # Add trailing / if not there
