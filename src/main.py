22ddedf2cb04090b6d337c0e4dcab8db0dcbdea5
Bijan Haney <bijanhaney@gmail.com>
2025-07-03 15:50:54 -0400

    Update main.py to change SUBTYPE to API (#10)

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ async def main():
     """
 
     Registry.register_resource_creator(
-        MLModel.SUBTYPE,
+        MLModel.API,
         TensorflowModule.MODEL,
         ResourceCreatorRegistration(
             TensorflowModule.new_service, TensorflowModule.validate_config
@@ -22,7 +22,7 @@ async def main():
     )
     module = Module.from_args()
 
-    module.add_model_from_registry(MLModel.SUBTYPE, TensorflowModule.MODEL)
+    module.add_model_from_registry(MLModel.API, TensorflowModule.MODEL)
     await module.start()
 
 
