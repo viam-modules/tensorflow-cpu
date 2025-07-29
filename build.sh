2e2199c7992db5ee0d9ed3ac063313ffc3a7ec1d
Alan Davidson <alan@viam.com>
2025-07-29 11:58:10 -0400

    put quotes around echoed things, else you can run into trouble with punctuation

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 SUDO=sudo
 if ! command -v $SUDO; then
-    echo no sudo on this system, proceeding as current user
+    echo "no sudo on this system, proceeding as current user"
     SUDO=""
 fi
 
@@ -16,21 +16,21 @@ if command -v apt-get; then
         echo "python3-venv is installed, skipping setup"
     else
         if ! apt info python3-venv; then
-            echo python3-venv package info not found, trying apt update
+            echo "python3-venv package info not found, trying apt update"
             $SUDO apt-get -qq update
         fi
         $SUDO apt-get install -qqy python3-venv
     fi
 else
-    echo Skipping tool installation because your platform is missing apt-get.
-    echo If you see failures below, install the equivalent of python3-venv for your system.
+    echo "Skipping tool installation because your platform is missing apt-get."
+    echo "If you see failures below, install the equivalent of python3-venv for your system."
 fi
 
 source .env
-echo creating virtualenv at $VIRTUAL_ENV
+echo "creating virtualenv at $VIRTUAL_ENV"
 python3 -m venv $VIRTUAL_ENV
-echo installing dependencies from requirements.txt
+echo "installing dependencies from requirements.txt"
 $VIRTUAL_ENV/bin/pip install --prefer-binary -r requirements.txt -U
 source $VIRTUAL_ENV/bin/activate
-$PYTHON -m PyInstaller --onefile --hidden-import="googleapiclient" --add-data="./src:src"  src/main.py
+$PYTHON -m PyInstaller --onefile --hidden-import="googleapiclient" --add-data="./src:src" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
