4506980aa700741652f7d7d303bff7a982ca085c
Alan Davidson <alan@viam.com>
2025-08-04 15:05:30 -0400

    bugfix, perhaps

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ echo "creating virtualenv at $VIRTUAL_ENV"
 python3 -m venv $VIRTUAL_ENV
 
 echo "installing dependencies from requirements.txt"
-if -f $VIRTUAL_ENV/bin/activate; then # Linux
+if [ -f $VIRTUAL_ENV/bin/activate ]; then # Linux
     source $VIRTUAL_ENV/bin/activate
 else
     source $VIRTUAL_ENV/Scripts/activate # Windows
