a74a94d184e29be66c4b02829ded9f1e5cf03f07
Alan Davidson <alan@viam.com>
2025-08-04 15:40:22 -0400

    [RSDK-11469] Add Windows support to the CI (#17)
    and other refactoring:
    - When you run `make setup`, the script that runs is now called `setup.sh`, and it does setup work.
    - There's a new `make dist/main` target that builds the executable, separate from the setup stuff
    - There's a new step in `publish.yml` to build and upload on Windows. I've marked the file as one to run on `workflow_dispatch` so you can manually run it without creating a release, and have [gotten that to work](https://github.com/viam-modules/tensorflow-cpu/actions/runs/16654086812).
    - The requirements.txt did not pin versions of stuff, and it rotted and could not find mutually compatible versions of everything. So, I copied the versions pinned in https://github.com/viam-modules/keras-detector/. I was going to pin all versions so this wouldn't happen again, and got surprised that `pip freeze` gives no output! This happens on both Windows and Linux. So, I gave up on that for the moment.
    - Tried out: everything builds and uploads correctly, and the Windows version appears to run on a Windows machine.
    
    
    * remove trailing whitespace
    
    * put quotes around echoed things, else you can run into trouble with punctuation
    
    * the installer script just installs things
    
    * name the setup script correctly
    
    * fiddling with the workflows, not done
    
    * cribbing off of the meeting with Abe and Khari earlier today
    
    * whoops: setup-python is on v5 these days
    
    * make the setup script more verbose to help diagnose what goes wrong
    
    * tar the right files on Windows
    
    * REVERTME: get the workflow_dispatch to run
    
    * debugging on Windows
    
    * try to resolve dependency mismatch
    
    * debugging
    
    * re-enable release version validation
    
    * pr feedback: mark setup as a phony make target
    
    * remove duplicated code
    
    * and refactor out the duplicated 'source' command, too
    
    * bugfix, perhaps
    
    * bugfix

--- Makefile
+++ Makefile
@@ -1,11 +1,28 @@
-setup:
-	./build.sh
-	
+SHELL := /bin/bash
+
+.setup: setup.sh
+	./setup.sh
+
+.PHONY: setup
+setup: .setup
+
 test:
 	PYTHONPATH=./src pytest
-	
-dist/archive.tar.gz:
+
+ifeq ($(OS),Windows_NT)
+ACTIVATE = .venv/Scripts/activate
+else
+ACTIVATE = .venv/bin/activate
+endif
+dist/main: .setup src/*
+	source $(ACTIVATE) && python -m PyInstaller --onefile --hidden-import="googleapiclient" --add-data="./src:src" src/main.py
+
+dist/archive.tar.gz: dist/main
+ifeq ($(OS),Windows_NT)
+	tar -czvf dist/archive.tar.gz dist/main.exe
+else
 	tar -czvf dist/archive.tar.gz dist/main
+endif
 
 lint:
 	pylint --disable=C0114,C0115,C0116,C0103,C0303,C0200,W0719,W4902,W0212,C0301,E1101,E0401,W1201,W0201,W0613 src/
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# setup.sh -- environment bootstrapper for python virtualenv
+
+set -exuo pipefail
+
+SUDO=sudo
+if ! command -v $SUDO; then
+    echo "no sudo on this system, proceeding as current user"
+    SUDO=""
+fi
+
+
+if command -v apt-get; then
+    $SUDO apt-get -y install python3-venv
+    if dpkg -l python3-venv; then
+        echo "python3-venv is installed, skipping setup"
+    else
+        if ! apt info python3-venv; then
+            echo "python3-venv package info not found, trying apt update"
+            $SUDO apt-get -qq update
+        fi
+        $SUDO apt-get install -qqy python3-venv
+    fi
+else
+    echo "Skipping tool installation because your platform is missing apt-get."
+    echo "If you see failures below, install the equivalent of python3-venv for your system."
+fi
+
+source .env
+echo "creating virtualenv at $VIRTUAL_ENV"
+# WARNING: As of August 2025, Tensorflow is not available in Python 3.13. If you get stuck here,
+# consider using python3.12 instead.
+python3 -m venv $VIRTUAL_ENV
+
+echo "installing dependencies from requirements.txt"
+if [ -f $VIRTUAL_ENV/bin/activate ]; then # Linux
+    source $VIRTUAL_ENV/bin/activate
+else
+    source $VIRTUAL_ENV/Scripts/activate # Windows
+fi
+pip install --prefer-binary -r requirements.txt -U
+
+touch .setup
