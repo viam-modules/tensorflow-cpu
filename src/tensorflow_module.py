82ae28eea6eeb60b1cf94f3409bd0377546f6326
Vignesh <vpandiarajan20@gmail.com>
2025-08-14 12:00:45 -0400

    address few comments

--- src/tensorflow_module.py
+++ src/tensorflow_module.py
@@ -190,14 +190,13 @@ class TensorflowModule(MLModel, Reconfigurable):
         try:
             # Attempt direct call with raw data
             res = self.model(data)
-        except (ValueError, TypeError, RuntimeError) as direct_err:
+        except (ValueError, TypeError) as direct_err:
             try:
                 f = self.model.signatures["serving_default"]
-                input_tensor = tf.convert_to_tensor(data, dtype=tf.float32)
                 _, kwargs_spec = f.structured_input_signature
                 input_name = next(iter(kwargs_spec.keys()))
-                res = f(**{input_name: input_tensor})
-            except (ValueError, TypeError, RuntimeError) as serving_err:
+                res = f(**{input_name: data})
+            except (ValueError, TypeError) as serving_err:
                 raise Exception(
                     f"both direct model inference and serving_default failed: direct_err={direct_err}; serving_default_err={serving_err}"
                 ) from serving_err
