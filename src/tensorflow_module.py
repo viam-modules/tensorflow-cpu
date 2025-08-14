8c710dc9e265adc35dc906d0bd922d492312d930
Vignesh <vpandiarajan20@gmail.com>
2025-08-14 15:56:37 -0400

    remove some logs

--- src/tensorflow_module.py
+++ src/tensorflow_module.py
@@ -211,12 +211,10 @@ class TensorflowModule(MLModel, Reconfigurable):
             res = self.model.predict(data, verbose=0)
             return {"output_0": np.asarray(res)}
 
-        LOGGER.info("data: %s", data)
         # Normalize data if values are greater than 1.0 (typical for uint8 images)
         if np.any(data > 1.0):
             LOGGER.info("Normalizing data")
             data = data / 255.0
-        LOGGER.info("data: %s", data)
 
         res = self._run_inference(data)
         return self._process_outputs(res)
