680e6f853adefa9cb5a262a550baf2908b7d5528
Adwait Ganguly <adwait.ganguly@viam.com>
2025-08-19 11:12:49 -0400

    python3.10 venv

--- setup.sh
+++ setup.sh
@@ -11,26 +11,26 @@ fi
 
 
 if command -v apt-get; then
-    $SUDO apt-get -y install python3.10
-    if dpkg -l python3.10-venv; then
-        echo "python3.10-venv is installed, skipping setup"
+    $SUDO apt-get -y install python3-venv
+    if dpkg -l python3-venv; then
+        echo "python3-venv is installed, skipping setup"
     else
-        if ! apt info python3.10-venv; then
-            echo "python3.10-venv package info not found, trying apt update"
+        if ! apt info python3-venv; then
+            echo "python3-venv package info not found, trying apt update"
             $SUDO apt-get -qq update
         fi
-        $SUDO apt-get install -qqy python3.10-venv
+        $SUDO apt-get install -qqy python3-venv
     fi
 else
     echo "Skipping tool installation because your platform is missing apt-get."
-    echo "If you see failures below, install the equivalent of python3.10-venv for your system."
+    echo "If you see failures below, install the equivalent of python3-venv for your system."
 fi
 
 source .env
 echo "creating virtualenv at $VIRTUAL_ENV"
 # WARNING: As of August 2025, Tensorflow is not available in Python 3.13. If you get stuck here,
 # consider using python3.12 instead.
-python3 -m venv $VIRTUAL_ENV
+python3.10 -m venv $VIRTUAL_ENV
 
 echo "installing dependencies from requirements.txt"
 if [ -f $VIRTUAL_ENV/bin/activate ]; then # Linux
